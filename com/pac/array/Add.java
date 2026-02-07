e4b6183ca6f00e2a252c28efa739299c85ce9b41
Abhinav <singhabhinav8074@gmail.com>
2026-02-07 18:47:14 +0530

    Add

---
+++ com/pac/array/Add.java
@@ -0,0 +1,14 @@
+package com.pac.array;
+
+
+public class Add {
+    public static int add(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(10,19));
+
+    }
+}
